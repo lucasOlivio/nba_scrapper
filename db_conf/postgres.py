dea792dde4cacea625bb5940dbb88d20785b7e64
lucasOlivio <lucas27_olivio@hotmail.com>
2019-09-07 11:11:45 -0300

    Saving players info

---
+++ db_conf/postgres.py
@@ -0,0 +1,190 @@
+import psycopg2
+
+class PostDB():
+
+    def __init__(self):
+        """ Initializes the DB, check if the folder has the configuration file first """
+        try:
+            from config import config
+        except Exception as e:
+            print('Create a database.ini with the configurations and proper format before continuing...')
+            print('Error -> ', e)
+            exit()
+
+        self.__conn = None
+        self.__connected = False
+
+        # read connection parameters
+        self.__params = config()
+
+    def isConnected(self):
+        """ Check if the DB is connected """
+        return self.__connected
+    
+    def checkDB(self):
+        """ Check if the DB exists """
+
+        test = False
+        try:
+            test = self.connectDB()
+
+            cur = self.__conn.cursor()
+
+            print('PostgreSQL database version:')
+            cur.execute('SELECT version()')
+
+            db_version = cur.fetchone()
+            print(db_version)
+
+            cur.close()
+        except (Exception, psycopg2.DatabaseError) as e:
+            print('Error -> ', e)
+        finally:
+            self.closeDB()
+
+            return test
+    
+    def dropDB(self):
+        from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
+
+        try:
+            # Drops the database in the database.ini conf
+            answer = input('THIS COMMAND WILL DROP THE WHOLE DATABASE AND ALL THE DATA IN IT, ARE YOU SURE WANT TO PROCEED? (TYPE: "I DO" TO DROP)')
+
+            if answer == 'I DO':
+                print('Dropping '+self.__params['database']+' database...')
+                con = psycopg2.connect(dbname='postgres',
+                user=self.__params['user'], host='',
+                password=self.__params['password'])
+
+                con.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
+
+                cur = con.cursor()
+                cur.execute("DROP DATABASE %s  ;" % self.__params['database'])
+
+        except (Exception, psycopg2.DatabaseError) as e:
+            print('Error -> ', e)
+    
+    def createDB(self):
+        from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
+
+        try:
+            # Creates the database and connects to it in case of success
+            print('Creating '+self.__params['database']+' database...')
+            con = psycopg2.connect(dbname='postgres',
+            user=self.__params['user'], host='',
+            password=self.__params['password'])
+
+            con.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
+
+            cur = con.cursor()
+            cur.execute("CREATE DATABASE %s  ;" % self.__params['database'])
+
+        except (Exception, psycopg2.DatabaseError) as e:
+            print('Error -> ', e)
+        finally:
+            return self.checkDB()
+
+    def connectDB(self):
+        try:
+            # Connect to the database
+            print('Connecting to '+self.__params['database']+' database...')
+            self.__conn = psycopg2.connect(**self.__params)
+
+            self.__connected = True
+        except (Exception, psycopg2.DatabaseError) as e:
+            print('Error -> ', e)
+        finally:
+            return self.isConnected()
+    
+    def closeDB(self):
+        try:
+            # Close the database
+            if self.__conn is not None:
+                self.__conn.close()
+                print('Database connection closed.')
+            
+            self.__connected = False
+            self.__conn = None
+        except (Exception, psycopg2.DatabaseError) as e:
+            print('Error -> ', e)
+        finally:
+            return self.isConnected()
+
+    def importSQL(self, sqlFile, shell = False):
+        # Execute sql file
+        check = False
+        print('Importing sql file...')
+        if shell:
+            # Runs the sql file directly with psql in shell (Better for some commands like COPY...FROM STDIN)
+            import subprocess
+
+            try:
+                subprocess.call('psql '+self.__params['database']+' < '+sqlFile, shell=True)
+                check = True
+            except (Exception, psycopg2.DatabaseError) as e:
+                print('Error -> ', e)
+            finally:
+                return check
+        else:
+            if self.connectDB():
+                self.__conn.autocommit = True
+
+                cur = self.__conn.cursor()
+
+                try:
+                    with open(sqlFile, "r") as sql:
+                        code = sql.read()
+                        cur.execute(code)
+                        check = True
+                except (Exception, psycopg2.DatabaseError) as e:
+                    print('Error -> ', e)
+                    sql.close()
+                finally:
+                    self.closeDB()
+                    return check
+            else:
+                print('There was a problem connecting to the database, check it and try again...')
+                return check
+    
+    def select(self, columns, tables, conditions="1", complement=""):
+        result = []
+
+        try:
+            if self.connectDB():
+                SQL    = "SELECT {} FROM {} WHERE (%s) {}".format(columns, tables, complement)
+                VALUES = (conditions,)
+
+                cur = self.__conn.cursor()
+                cur.execute(SQL, VALUES)
+
+                result = cur.fetchall()
+            else:
+                print('There was a problem connecting to the database, check it and try again...')
+        except (Exception, psycopg2.DatabaseError) as e:
+            print('Error -> ', e)
+        finally:
+            self.closeDB()
+
+            return result
+    
+    def insert(self, columns, tables, rows):
+
+        id = -1
+        try:
+            if self.connectDB():
+                SQL = 'INSERT INTO {} ({}) VALUES %s'.format(tables, columns)
+                VALUES = (conditions,)
+
+                cur = self.__conn.cursor()
+                cur.execute(SQL, VALUES)
+
+                id = cur.fetchone()[0]
+            else:
+                print('There was a problem connecting to the database, check it and try again...')
+        except (Exception, psycopg2.DatabaseError) as e:
+            print('Error -> ', e)
+        finally:
+            self.closeDB()
+
+            return id
--- scrap.py
+++ scrap.py
@@ -1,20 +1,74 @@
 from selenium import webdriver
-from time import sleep
+import sys, os, csv
+sys.path.insert(0, './db_conf')
 
-link = 'https://stats.nba.com/players/list/?Historic=Y'
+from postgres import PostDB
+
+home = 'https://stats.nba.com/players/list/?Historic=Y'
 
 browser = webdriver.Chrome(executable_path="./chromedriver")
-browser.get(link)
 
-sleep(3)
+browser.get(home)
 
-print('Searching players...')
+db = PostDB()
 
+# Test with DB
+# if not db.checkDB():
+#     if db.createDB():
+#         if not db.importSQL('./db_conf/players_table.sql'):
+#             db.dropDB()
+
+print('Searching players IDs...')
+# Gets every player id on the page
+links_list = []
 players_list = browser.find_elements_by_xpath("//li[@class='players-list__name']//a")
-
 for player_item in players_list:
-    player_item.click()
+    links_list.append(player_item.get_attribute('href'))
 
-    player_data = browser.find_elements_by_xpath("//div[@class='player-summary__player-name']")
 
-    print(player_data.text)
+
+print('Searching players data...')
+# Iterate over the players ID list to access his respective page and get all info
+folder = sys.argv[1]+'/'
+for link in links_list:
+    # Access his page
+    browser.get(link)
+    # Get player name and format it for the folder name
+    name = browser.find_elements_by_xpath('//div[@class="player-summary__player-name"]')
+    formated_name = ' '.join(name[0].text.split('\n'))
+    player_folder = folder+formated_name+'/'
+    # Checks if the folder already exists before creation
+    if not os.path.exists(player_folder):
+        os.makedirs(player_folder)
+    # Find the table where all the statistics tables are
+    stats_tables = browser.find_elements_by_xpath('//nba-stat-table')
+    # Iterate over each table and create your own csv file
+    table = []
+    for index_table, stats_table in enumerate(stats_tables):
+        table = []
+        caption = []
+        # Get the table name
+        caption_item = stats_table.find_elements_by_xpath('.//div[@class="nba-stat-table__caption"]')
+        caption = 'Table'+str(index_table) if len(caption_item) == 0 else caption_item[0].text
+        # Get the columns names
+        head = []
+        col_list = stats_table.find_elements_by_xpath('.//div[@class="nba-stat-table__overflow"]/table/thead/tr/th')
+        for index, col in enumerate(col_list):
+            head.append(col.text)
+        table.append(head)
+        # Get the values of each row
+        row_list = stats_table.find_elements_by_xpath('.//div[@class="nba-stat-table__overflow"]/table/tbody/tr')
+        for index_row, row in enumerate(row_list):
+            body = []
+            col_list = row.find_elements_by_xpath('.//td')
+            for index_col, row_col in enumerate(col_list):
+                body.append(row_col.text)
+            table.append(body)
+        # Create a new csv file and save all information there
+        with open(player_folder+caption+'.csv', 'w') as f:
+            wr = csv.writer(f, dialect='excel')
+            wr.writerows(table)
+
+# Closes the browser
+print('Data colected, closing driver...')
+browser.close()
